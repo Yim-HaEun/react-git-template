3c16b814913a8f57a294557fc0b19cbc0999c714
Yim-HaEun <49932613+Yim-HaEun@users.noreply.github.com>
2023-12-26 11:22:55 +0900

    client/server frontend/backend

---
+++ node-react-todoList/client/src/App.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function App() {
+  const [todos, setTodos] = useState([]);
+
+  //npm i axios
+  /*useEffect(() => {
+    //추후 server에서 Express를 활용해서 todos 데이터 가져올 것
+    fetch('/')
+      .then((res) => res.json())
+      .then((data) => setTodos(data))
+      .catch((error) => console.error('에러가 발생했습니다.', error));
+  });*/
+  useEffect(() => {
+    axios
+      .get('/')
+      .then((response) => setTodos(response.data))
+      .catch((error) => console.error('에러가 발생했습니다.', error));
+  }, []);
+
+  return (
+    <div>
+      <h1>투두리스트</h1>
+      <ul>
+        <li>
+          {todos.map((todo) => {
+            <li key={todo.ID}> {todo.TASK}</li>;
+          })}
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
---
+++ node-react-todoList/server/express.md
@@ -0,0 +1,9 @@
+express는 Node.js를 사용해서 백엔드 서버를 구축하고 데이터베이스와 통신할 수 있게 해주는 프레임워크
+
+express는 npm i express나 npm install express 를 사용해 설치할 수 있음
+
+cd server
+npm init
+npm i oracledb express
+
+현재는 데이터베이스를 오라클데이터베이스를 사용하기 때문에 npm install oracledb를 추가로 설치해준 것
